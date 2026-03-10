747741a9ad7f530646de4223304759ca73e7cc94
C-H-Z <3262549304@qq.com>
2026-03-11 00:51:22 +0800

    Implement event-driven loot dropping mechanism
    Refactor DropLoot class to use event subscription for loot dropping on enemy defeat.

--- Scripts/NPCSripts/DropLoot.cs
+++ Scripts/NPCSripts/DropLoot.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class DropLoot : MonoBehaviour
 {
-    public Animator anim;
     public GameObject Prefab;
     public ItemSO ItemData;
     public int quantity = 3;
 
-    public void Droploot()
+    private Enemy_Health myEnemyHealth;
+
+    private void Awake()
     {
-        GameObject lootObject = Instantiate(Prefab, transform.position, Quaternion.identity);
-        Loot loot = lootObject.GetComponent<Loot>();
-        loot.Initialize(ItemData, quantity);
+        myEnemyHealth = GetComponent<Enemy_Health>();
+    }
+    private void OnEnable()
+    {
+        if (myEnemyHealth != null)
+        {
+            myEnemyHealth.OnThisMonsterDefeated += Droploot;
+        }
+    }
+    private void OnDisable()
+    {
+        if (myEnemyHealth != null)
+        {
+            myEnemyHealth.OnThisMonsterDefeated -= Droploot;
+        }
     }
 
+    private void Droploot(int exp)
+    {
+
+            GameObject lootObject = Instantiate(Prefab, transform.position, Quaternion.identity);
+            Loot loot = lootObject.GetComponent<Loot>();
+            loot.Initialize(ItemData, quantity);
+
+    }
 }
